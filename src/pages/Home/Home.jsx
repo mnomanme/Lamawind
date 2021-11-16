0eaa342f3c5238441ee34c7dfda234924ccc38bc
nomanme <abdullah.noman083@gmail.com>
2021-11-17 00:05:12 +0600

    slider pass

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import { Announcement } from '../../components/Announcement/Announcement';
 import { Navbar } from '../../components/Navbar/Navbar';
+import { Slider } from '../../components/Slider/Slider';
 
 export const Home = () => {
 	return (
 		<>
 			<Announcement />
 			<Navbar />
+			<Slider />
 		</>
 	);
 };
