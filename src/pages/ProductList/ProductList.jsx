bc54cbd44ef05e1efbab4ba19b1bc09a5d1f0b2b
nomanme <abdullah.noman083@gmail.com>
2021-11-20 23:56:24 +0600

    product list done

--- src/pages/ProductList/ProductList.jsx
+++ src/pages/ProductList/ProductList.jsx
@@ -1,7 +1,84 @@
 import styled from 'styled-components';
+import { Announcement } from '../../components/Announcement/Announcement';
+import { Footer } from '../../components/Footer/Footer';
+import { Navbar } from '../../components/Navbar/Navbar';
+import { NewsLetter } from '../../components/NewsLetter/NewsLetter';
+import { Products } from '../../components/Products/Products';
 
 const Container = styled.div``;
 
+const Title = styled.h1`
+	margin: 20px;
+`;
+
+const FilterContainer = styled.div`
+	display: flex;
+	justify-content: space-between;
+`;
+
+const Filter = styled.div`
+	margin: 20px;
+`;
+
+const FilterText = styled.span`
+	font-style: 200px;
+	font-weight: 600;
+	margin-right: 20px;
+`;
+
+const Select = styled.select`
+	padding: 10px;
+	margin-right: 20px;
+`;
+
+const Option = styled.option``;
+
 export const ProductList = () => {
-	return <Container>This is Product List</Container>;
+	return (
+		<Container>
+			<Navbar />
+			<Announcement />
+			<Title>Dresses</Title>
+			<FilterContainer>
+				<Filter>
+					<FilterText>Filter Products:</FilterText>
+					<Select>
+						<Option disabled selected>
+							Color
+						</Option>
+						<Option>White</Option>
+						<Option>Black</Option>
+						<Option>Red</Option>
+						<Option>Blue</Option>
+						<Option>Yellow</Option>
+						<Option>Green</Option>
+						<Option>Pink</Option>
+					</Select>
+					<Select>
+						<Option disabled selected>
+							Size
+						</Option>
+						<Option>Xs</Option>
+						<Option>S</Option>
+						<Option>M</Option>
+						<Option>M</Option>
+						<Option>L</Option>
+						<Option>XL</Option>
+						<Option>XXL</Option>
+					</Select>
+				</Filter>
+				<Filter>
+					<FilterText>Sort Products:</FilterText>
+					<Select>
+						<Option selected>Newest</Option>
+						<Option>Price (asc)</Option>
+						<Option>Price (desc)</Option>
+					</Select>
+				</Filter>
+			</FilterContainer>
+			<Products />
+			<NewsLetter />
+			<Footer />
+		</Container>
+	);
 };
