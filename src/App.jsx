a2e5a2066a3c3ba68012823cb676d5779bbdc6ef
nomanme <abdullah.noman083@gmail.com>
2021-11-21 00:40:55 +0600

    pass product page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import { Home } from './pages/Home/Home';
+import { Product } from './pages/Product/Product';
 import { ProductList } from './pages/ProductList/ProductList';
 
 const App = () => {
 	return (
 		<>
 			{/* <Home /> */}
-			<ProductList />
+			{/* <ProductList /> */}
+			<Product />
 		</>
 	);
 };
