81b5b4d5d70db80f2693530bce4f48c5c1da6c12
nomanme <abdullah.noman083@gmail.com>
2021-11-21 23:12:50 +0600

    responsive navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,12 @@
 import styled from 'styled-components';
 import { Badge } from '@material-ui/core';
 import { Search, ShoppingCartOutlined } from '@material-ui/icons';
+import { mobile } from '../../responsive';
 
 export const Navbar = () => {
 	const Container = styled.div`
 		height: 60px;
+		${mobile({ height: '50px' })}
 	`;
 
 	const Wrapper = styled.div`
@@ -12,6 +14,7 @@ export const Navbar = () => {
 		display: flex;
 		align-items: center;
 		justify-content: space-between;
+		${mobile({ padding: '10px 0px' })}
 	`;
 
 	const Left = styled.div`
@@ -30,11 +33,13 @@ export const Navbar = () => {
 		display: flex;
 		align-items: center;
 		justify-content: flex-end;
+		${mobile({ flex: 2, justifyContent: 'center' })}
 	`;
 
 	const Language = styled.div`
 		font-size: 14px;
 		cursor: pointer;
+		${mobile({ display: 'none' })}
 	`;
 
 	const SearchContainer = styled.div`
@@ -47,16 +52,19 @@ export const Navbar = () => {
 
 	const Input = styled.input`
 		border: none;
+		${mobile({ width: '50px' })}
 	`;
 
 	const Logo = styled.h1`
 		font-weight: bold;
+		${mobile({ fontSize: '24px' })}
 	`;
 
 	const MenuItem = styled.div`
 		font-size: 14px;
 		cursor: pointer;
 		margin-left: 25px;
+		${mobile({ fontSize: '12px', marginLeft: '10px' })}
 	`;
 
 	return (
@@ -65,12 +73,12 @@ export const Navbar = () => {
 				<Left>
 					<Language>EN</Language>
 					<SearchContainer>
-						<Input />
+						<Input placeholder="search" />
 						<Search style={{ color: 'gray', fontSize: '16px' }} />
 					</SearchContainer>
 				</Left>
 				<Center>
-					<Logo>LAMAWIND.</Logo>
+					<Logo>LAMA.</Logo>
 				</Center>
 				<Right>
 					<MenuItem>REGISTER</MenuItem>
