5070993292dcbbdece624a96818122a2b98b8ab3
nomanme <abdullah.noman083@gmail.com>
2021-11-21 14:24:26 +0600

    pass register to app components

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import './App.css';
 import { Home } from './pages/Home/Home';
 import { Product } from './pages/Product/Product';
 import { ProductList } from './pages/ProductList/ProductList';
+import { Register } from './pages/Register/Register';
 
 const App = () => {
 	return (
 		<>
 			{/* <Home /> */}
 			{/* <ProductList /> */}
-			<Product />
+			{/* <Product /> */}
+			<Register/>
 		</>
 	);
 };
